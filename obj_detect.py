0e9b05dbac82cc57a08ebc873a4e668d64bde447
reqmd <damil.kulikov.1100@gmail.com>
2026-07-20 15:46:16 +0300

    edit obj_detect.py

--- obj_detect.py
+++ obj_detect.py
@@ -1,13 +1,13 @@
-import numpy
+import numpy as np
 import cv2
+import os
 
-image_path = 'data/orig_images/image_20-07-2026_10-59-25.png'
-image = cv2.imread(image_path, flags=cv2.COLOR_BGR2RGB)
+image_path = 'data/orig_images/image_20-07-2026_15-19-22.png'
+obj_path = 'data/objects'
 
-
-
-
-
+image = cv2.imread(image_path, flags=cv2.COLOR_BGR2GRAY)
+if not os.path.exists(obj_path):
+    os.mkdir(obj_path)
 
 cv2.imshow('image', image)
 cv2.waitKey(0)
