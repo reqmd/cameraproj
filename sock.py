6de53710994558d60d5016482423c4ab8f2f9f44
reqmd <damil.kulikov.1100@gmail.com>
2026-07-23 11:17:58 +0300

    add find_objects

--- sock.py
+++ sock.py
@@ -6,6 +6,7 @@ import time
 import datetime
 import os
 from resize import upscale
+from obj_detect import find_objects
 
 CAMERA_IP   = "192.168.2.101"   
 CAMERA_PORT = 5001             
@@ -15,8 +16,9 @@ cmd = bytes.fromhex("01 00 03 00 00 00 6A 00 00 1F D4 8e 06")
 data_path = 'data'
 resized_images_path = 'data/resized_images'
 orig_images_path = 'data/orig_images'
+obj_path = 'data/objects'
 num = 256
-need_resize = True
+need_resize = False
 sleeep = 0
 image_full = []
 
@@ -49,15 +51,20 @@ sock.close()
 image_full = np.array(image_full, dtype=np.uint8).reshape(num, -1, 3)
 image_full = cv2.cvtColor(image_full, cv2.COLOR_BGR2RGB)
 print(image_full.shape)
-end_time = time.time()
-print('Конец работы:', (end_time - start_time))
+
 date = datetime.datetime.now()
 formatted_string_ru = date.strftime("%d-%m-%Y_%H-%M-%S")
 
-str_orig = os.path.join(orig_images_path, f"image_{formatted_string_ru}.png")
+str_image = f"image_{formatted_string_ru}.png"
+
+str_orig = os.path.join(orig_images_path,str_image )
 cv2.imwrite(str_orig, image_full)
+find_objects(image_full, str_image)
 
 if need_resize:
     image_resized = upscale(image_full)
     str_resized = os.path.join(resized_images_path, f"image_resized_{formatted_string_ru}.png")
     cv2.imwrite(str_resized, image_resized)
+
+end_time = time.time()
+print('Конец работы:', (end_time - start_time))
